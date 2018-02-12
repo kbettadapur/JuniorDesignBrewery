d7c1ff01eab4a07dccb51e0e93b2699193188ee8
kbettadapur <august.wagner@live.com>
2018-02-12 17:27:26 -0500

    Brewery and List View fixes

--- components/RegisterScreen.js
+++ components/RegisterScreen.js
@@ -1,3 +1,24 @@
+/*
+* Registration Screen from the Family Friendly Brewery Tracker
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License version 3 as
+* published by the Free Software Foundation;
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS.
+* IN NO EVENT SHALL THE COPYRIGHT HOLDER(S) AND AUTHOR(S) BE LIABLE FOR ANY
+* CLAIM, OR ANY SPECIAL INDIRECT OR CONSEQUENTIAL DAMAGES, OR ANY DAMAGES
+* WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+* ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+* OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+*
+* ALL LIABILITY, INCLUDING LIABILITY FOR INFRINGEMENT OF ANY PATENTS,
+* COPYRIGHTS, TRADEMARKS OR OTHER RIGHTS, RELATING TO USE OF THIS
+* SOFTWARE IS DISCLAIMED.
+*/
+
 import React from 'react';
 import { 
   StyleSheet, 
@@ -33,18 +54,18 @@ export class RegisterScreen extends React.Component {
     return (
       <View style={styles.container}>
         <TextInput
-          style={{height: 40, width: 100,borderColor: 'gray', borderWidth: 0}}
+          style={styles.textinput}
           onChangeText={(email) => this.setState({email})}
           value={this.state.email} 
           placeholder="Email" />
         <TextInput
-          style={{height: 40, width: 100,borderColor: 'gray', borderWidth: 0}}
+          style={styles.textinput}
           onChangeText={(password) => this.setState({password})}
           value={this.state.password} 
-          placeholder="password" />
+          placeholder="Password" />
 
         <TextInput
-          style={{height: 40, width: 100,borderColor: 'gray', borderWidth: 0}}
+          style={styles.textinput}
           onChangeText={(username) => this.setState({username})}
           value={this.state.username} 
           placeholder="Username" />
@@ -101,5 +122,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  textinput: {
+    height: 58,
+    fontSize: 18, 
+    minWidth: '75%',
+    maxWidth: '75%', 
+    marginTop: 5,
+    marginBottom: 5,
+    borderColor: 'gray', 
+    borderWidth: 0
   },
 });
