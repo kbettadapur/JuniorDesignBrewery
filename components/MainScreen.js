d62d8badf40612a24acc3482a884d0ab045e775d
August Wagner <august.wagner@live.com>
2018-04-04 06:05:31 -0400

    Favorites list and distance sorting

--- components/MainScreen.js
+++ components/MainScreen.js
@@ -145,73 +145,34 @@ export class MainScreen extends React.Component {
       <Container>
         <Footer>
             <FooterTab tabActiveBgColor="#FFFFF">
-                {this.state.selectedTab == MAP_TAB && <Button
-                    active={this.state.selectedTab === MAP_TAB}
-                    onPress={() => this.changeTab(MAP_TAB)}
-                    style={{backgroundColor: '#76a8f7'}}
-                >
-                    <Icon name="md-beer"/>
-                    <Text>{"Breweries"}</Text>
-                </Button>}
-
-                {this.state.selectedTab != MAP_TAB && <Button
+                <Button
                     active={this.state.selectedTab === MAP_TAB}
                     onPress={() => this.changeTab(MAP_TAB)}
                 >
                     <Icon name="md-beer" />
                     <Text>{"Breweries"}</Text>
-                </Button>}
-
-                {this.state.selectedTab == FAVORITES_TAB && <Button
-                    active={this.state.selectedTab === FAVORITES_TAB}
-                    onPress={() => this.changeTab(FAVORITES_TAB)}
-                    style={{backgroundColor: '#76a8f7'}}
-                >
-                    <Icon name="star" />
-                    <Text>{"Favorites"}</Text>
-                </Button>}
-
-                {this.state.selectedTab != FAVORITES_TAB && <Button
+                </Button>
+                <Button
                     active={this.state.selectedTab === FAVORITES_TAB}
                     onPress={() => this.changeTab(FAVORITES_TAB)}
                 >
                     <Icon name="star" />
                     <Text>{"Favorites"}</Text>
-                </Button>}
-
-                {this.state.selectedTab == YOUR_REVIEWS_TAB && <Button
-                    active={this.state.selectedTab === YOUR_REVIEWS_TAB}
-                    onPress={() => this.changeTab(YOUR_REVIEWS_TAB)}
-                    style={{backgroundColor: '#76a8f7'}}
-                >
-                    <Icon name="list" />
-                    <Text>{"Your Reviews"}</Text>
-                </Button>}
-
-                {this.state.selectedTab != YOUR_REVIEWS_TAB && <Button
+                </Button>
+                <Button
                     active={this.state.selectedTab === YOUR_REVIEWS_TAB}
                     onPress={() => this.changeTab(YOUR_REVIEWS_TAB)}
                 >
                     <Icon name="list" />
                     <Text>{"Your Reviews"}</Text>
-                </Button>}
-
-                {this.state.selectedTab == PROFILE_TAB && <Button
-                    active={this.state.selectedTab === PROFILE_TAB}
-                    onPress={() => this.changeTab(PROFILE_TAB)}
-                    style={{backgroundColor: '#76a8f7'}}
-                >
-                    <Icon name="more" />
-                    <Text>{"Profile"}</Text>
-                </Button>}
-
-                {this.state.selectedTab != PROFILE_TAB && <Button
+                </Button>
+                <Button
                     active={this.state.selectedTab === PROFILE_TAB}
                     onPress={() => this.changeTab(PROFILE_TAB)}
                 >
                     <Icon name="more" />
                     <Text>{"Profile"}</Text>
-                </Button>}
+                </Button>
             </FooterTab>
         </Footer>
       </Container>
