9e3ab5a330a57a68299640bf700da3b98053bf98
Augdawg <august.wagner@live.com>
2018-04-20 00:01:20 -0400

    Removed firebase api key

--- firebase.js
+++ firebase.js
@@ -22,12 +22,12 @@
 import * as firebase from 'firebase';
 
 var config = {
-    apiKey: "AIzaSyBiAwA5G6xihHVUTv0XV5D2GPQYrKmsgdc",
-    authDomain: "brewerytracker.firebaseapp.com",
-    databaseURL: "https://brewerytracker.firebaseio.com",
-    projectId: "brewerytracker",
-    storageBucket: "brewerytracker.appspot.com",
-    messagingSenderId: "624606500424"
-  };
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
 
 export default firebaseApp = firebase.initializeApp(config);
